f7eb757df4236d8c1b1024808c3e9591ae602c37
Gabriel Brasil <bbrasil04@yahoo.com>
2021-12-28 14:08:07 -0300

    Filtro de Erro

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
+let contadorErros;
+
 document.getElementById('botao').addEventListener('click', function (event) {
-    event.preventDefault;
+    event.preventDefault();
 
     const nome = document.getElementById('nome').value;
     const monstro = document.getElementById('select_monstros').value;
@@ -8,27 +10,94 @@ document.getElementById('botao').addEventListener('click', function (event) {
     const local = document.getElementById('select_local').value;
     const acidente = document.getElementById('select_acidente').value;
 
-    if (nome === '' || monstro === 'Escolha uma opção' || especie === 'Escolha uma opção' || armamento === 'Escolha uma opção' || local === 'Escolha uma opção' || acidente === 'Escolha uma opção') {
-        alert('[ERRO] Formulário incompleto!');
-        document.querySelectorAll('input') // para apagar os valores que estão escritos no input
-            .forEach(function (input) {
-                input.value = '';
-            });
-    } else {
-        const dadosHistoria = { // objeto com os valores que os inputs receberam
-            nome: nome,
-            monstro: monstro,
-            especie: especie,
-            armamento: armamento,
-            local: local,
-            acidente: acidente,
-        };
+    const dadosHistoria = { // objeto com os valores que os inputs receberam
+        nome: nome,
+        monstro: monstro,
+        especie: especie,
+        armamento: armamento,
+        local: local,
+        acidente: acidente,
+    };
 
-        // modalResultados(dadosHistoria);
-    }
+    contadorErros = 0;
+    conferirFormulario(dadosHistoria)
 })
 
-// function erroFormulario () {
+function conferirFormulario(dados) {
 
-    
-// }
+    if (dados.nome === '') {
+        document.getElementById('nome').classList.add("erro");
+        document.getElementById('nome').classList.remove("correto");
+        document.getElementById('p_nome').innerHTML = 'Digite o nome';
+
+        contadorErros = contadorErros + 1;
+    } else {
+        document.getElementById('nome').classList.remove("erro");
+        document.getElementById('nome').classList.add("correto");
+        document.getElementById('p_nome').innerHTML = '';
+    }
+
+    if (dados.monstro === 'Escolha uma opção') {
+        document.getElementById('select_monstros').classList.add("erro");
+        document.getElementById('p_monstros').innerHTML = 'Escolha uma opção';
+
+        contadorErros = contadorErros + 1;
+    } else {
+        document.getElementById('select_monstros').classList.remove("erro");
+        document.getElementById('select_monstros').classList.add("correto");
+        document.getElementById('p_monstros').innerHTML = '';
+    }
+
+    if (dados.especie === 'Escolha uma opção') {
+        document.getElementById('select_especie').classList.add("erro");
+        document.getElementById('p_especie').innerHTML = 'Escolha uma opção';
+
+        contadorErros = contadorErros + 1;
+    } else {
+        document.getElementById('select_especie').classList.remove("erro");
+        document.getElementById('select_especie').classList.add("correto");
+        document.getElementById('p_especie').innerHTML = '';
+    }
+
+    if (dados.armamento === 'Escolha uma opção') {
+        document.getElementById('select_armamento').classList.add("erro");
+        document.getElementById('p_armamento').innerHTML = 'Escolha uma opção';
+
+        contadorErros = contadorErros + 1;
+    } else {
+        document.getElementById('select_armamento').classList.remove("erro");
+        document.getElementById('select_armamento').classList.add("correto");
+        document.getElementById('p_armamento').innerHTML = '';
+    }
+
+    if (dados.local === 'Escolha uma opção') {
+        document.getElementById('select_local').classList.add("erro");
+        document.getElementById('p_local').innerHTML = 'Escolha uma opção';
+
+        contadorErros = contadorErros + 1;
+    } else {
+        document.getElementById('select_local').classList.remove("erro");
+        document.getElementById('select_local').classList.add("correto");
+        document.getElementById('p_local').innerHTML = '';
+    }
+
+    if (dados.acidente === 'Escolha uma opção') {
+        document.getElementById('select_acidente').classList.add("erro");
+        document.getElementById('p_acidente').innerHTML = 'Escolha uma opção';
+
+        contadorErros = contadorErros + 1;
+    } else {
+        document.getElementById('select_acidente').classList.remove("erro");
+        document.getElementById('select_acidente').classList.add("correto");
+        document.getElementById('p_acidente').innerHTML = '';
+    }
+
+    conferirErros()
+}
+
+function conferirErros() {
+
+    if (contadorErros == 0) {
+        alert('tudo certo')
+    }
+}
